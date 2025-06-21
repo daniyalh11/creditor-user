43ba300bd4e2cc9283bca8d417aadbe201a049ae
NIkhil7048 <kumar@creditoracademy.com>
2025-06-21 12:34:13 +0530

    Delete src directory

--- src/utils/axios.js
+++
@@ -1,9 +0,0 @@
-// src/utils/axios.js
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:9000", // Match your backend port
-  withCredentials: true, // Send cookies with requests
-});
-
-export default api;
